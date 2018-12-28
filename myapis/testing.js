25a8cb151996789b9ee640cbf1d623af10159dff
DaimenAmbers <daimenambers@yahoo.com>
2018-12-27 23:40:16 -0500

    More updates on the code

--- myapis/testing.js
+++ myapis/testing.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
   //console.log(ApiKey.weatherApiKey);
-  $.get("https://api.twitter.com/1.1/account/verify_credentials.json", function (){
-    console.log(data);
-  });
 });
