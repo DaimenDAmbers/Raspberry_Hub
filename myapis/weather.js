13fabb3d22efcaa0dcb0d66394f3ad3951717817
DaimenAmbers <daimenambers@yahoo.com>
2018-12-29 20:03:03 -0500

    Able to now retweet, reply and favorite tweets. Still

--- myapis/weather.js
+++ myapis/weather.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
   console.log('Weather Doc Ready!');
 
-  $('#submit2').click(function () {
-    var URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/30318?apikey=" + WeatherApiKey.weatherApiKey
+    var URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/30318?apikey=" + ApiKey.weatherApiKey
     $.getJSON(URL, function (data) {
       console.dir(data);
       updateDOM(data);
     });
-  });
 
 });
 
