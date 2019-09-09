40cd0e05a38406a895de496cb34408a78ce47e2e
DRSM <daimenambers@yahoo.com>
2019-09-08 22:40:41 -0400

    Update access.py

--- access.py
+++ access.py
@@ -1,29 +1,146 @@
 import requests
 import base64
+import urllib
+import twitter_sign
+import json
 
-consumerKey = "dcbVy1h8Mrdd6ZhAX7KwYyx4a"
-consumerSecret = "JJQJe2OrgAu0nYaJYVrKmt2iiyQCGij9H7xifZvjJyt4ZKeWrp"
-accessToken = "1021945507516739584-pmEQangAGNfZC9hv2kIwBUR7Fy1sR0"
-tokenSecret = "RlOM9DRMG0WRpthCd7XehFR5Cy3qJBYfoVpWJ7gS6vFzr"
-basicAuth = "f1a0b8da-0c90-4a51-a26e-584d3bf9e6e8" #Not sure what this is anymore.
 
-key_secret = '{}:{}'.format(consumerKey, consumerSecret).encode('ascii')
-b64_encode_key = base64.b64encode(key_secret)
-b64_encode_key = b64_encode_key.decode('ascii')
+class TwitterAccess:
+    def __init__(self):
+        self.oauth_callback = 'https://daimenambersapp.com/callback'
+        self.url_encode_callback = urllib.quote(self.oauth_callback)
+        self.consumerKey = "dcbVy1h8Mrdd6ZhAX7KwYyx4a"
+        self.consumerSecret = "JJQJe2OrgAu0nYaJYVrKmt2iiyQCGij9H7xifZvjJyt4ZKeWrp"
+        self.accessToken = "1021945507516739584-pmEQangAGNfZC9hv2kIwBUR7Fy1sR0"
+        self.tokenSecret = "RlOM9DRMG0WRpthCd7XehFR5Cy3qJBYfoVpWJ7gS6vFzr"
+        self.basicAuth = "f1a0b8da-0c90-4a51-a26e-584d3bf9e6e8" #Not sure what this is anymore.
 
-base_url = 'https://api.twitter.com/'
-auth_url = '{}oauth2/token'.format(base_url)
-auth_headers = {
-    'Authorization': 'Basic {}'.format(b64_encode_key),
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    }
+        self.key_secret = '{}:{}'.format(self.consumerKey, self.consumerSecret).encode('ascii')
+        self.b64_encode_key = base64.b64encode(self.key_secret)
+        self.b64_decode_key = self.b64_encode_key.decode('ascii')
 
-auth_data = {
-    'grant_type': 'client_credentials'
-}
+        self.base_url = 'https://api.twitter.com/'
 
-auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-access_token = auth_resp.json()['access_token']
-# access_token = auth_resp.json()
-print(accessToken)
-print(auth_headers)
+        self.oauth_token = 'TGgRFAAAAAAA_5I1AAABbRNJI3g'
+        self.oauth_token_secret='goXPvI46rhofKOxXMTv9hRxfApsZ50g8'
+
+        # self.bearer_token = 'AAAAAAAAAAAAAAAAAAAAADWS%2FwAAAAAAF5NP0CqldNv2XvLeBcH%2B7ZH6xQo%3DcOOOwhYwsKq3i5M3H7aMLYu7rhT3p4bqnEcJ5pUthsznW7WQ9z'
+
+    def authorizeToken(self):
+        url = '{}oauth2/token'.format(self.base_url)
+        auth_headers = {
+            'Authorization': 'Basic {}'.format(self.b64_decode_key),
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            }
+
+        auth_data = {
+            'grant_type': 'client_credentials'
+        }
+
+        auth_resp = requests.post(url, headers=auth_headers, data=auth_data)
+        access_token = auth_resp.json()['access_token']
+        return access_token
+
+    def oauthRequest(self): #Not needed at the moment
+        method = "post"
+        url = '{}oauth/request_token'.format(self.base_url)
+        oauth_parameters = twitter_sign.get_oauth_parameters(self.consumerKey, self.accessToken)
+        url_parameters = {
+            'oauth_callback': self.url_encode_callback
+        }
+        oauth_parameters['oauth_signature'] = twitter_sign.generate_signature(method, url, url_parameters, oauth_parameters, self.consumerKey, self.consumerSecret, self.tokenSecret)
+        oauth_headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': twitter_sign.create_auth_header(oauth_parameters)}
+
+        oauth_params = {
+            'oauth_callback': self.url_encode_callback
+        }
+        print oauth_headers
+        oauth_resp = requests.post(url, headers=oauth_headers, params=oauth_params)
+        print oauth_resp.status_code
+
+
+    def oauthAuthorize(self): #Doesn't work at the moment
+        url = '{}oauth/authorize'.format(self.base_url)
+        params = {
+            'oauth_token': 'Inp3ygAAAAAA_5I1AAABbRLnomU'
+        }
+        response = requests.get(url, params=params)
+        print response
+        print response.status_code
+        # print response.json()
+
+    def newTweet(self):
+        method = "post"
+        url = '{}1.1/statuses/update.json'.format(self.base_url)
+        url_parameters = {
+            'status': raw_input('Tweet-> ')
+        }
+        oauth_parameters = twitter_sign.get_oauth_parameters(self.consumerKey, self.accessToken)
+        oauth_parameters['oauth_signature'] = twitter_sign.generate_signature(method, url, url_parameters, oauth_parameters, self.consumerKey, self.consumerSecret, self.tokenSecret)
+        headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': twitter_sign.create_auth_header(oauth_parameters)
+                }
+        response = requests.post(url, headers=headers, params=url_parameters)
+        print response.status_code
+
+    def curateStory(self):
+        method = "post"
+        url = '{}1.1/collections/create.json'.format(self.base_url)
+        url_parameters = {
+            'Name': raw_input('Description-> '),
+            'name': raw_input('Name of Collection-> '),
+            'description': raw_input('Description of Collection-> ')
+        }
+        oauth_parameters = twitter_sign.get_oauth_parameters(self.consumerKey, self.accessToken)
+        oauth_parameters['oauth_signature'] = twitter_sign.generate_signature(method, url, url_parameters, oauth_parameters, self.consumerKey, self.consumerSecret, self.tokenSecret)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': twitter_sign.create_auth_header(oauth_parameters)}
+        response = requests.post(url, headers=headers, params=url_parameters)
+        print response.status_code
+        print response.json()['response']['timeline_id']
+        timeline_id = response.json()['response']['timeline_id']
+        return timeline_id
+
+    def getCollection(self):
+        method = "get"
+        url = '{}1.1/collections/show.json'.format(self.base_url)
+        url_parameters = {
+            'Name': "C",
+            'id': 'custom-1170876786781097984'
+        }
+        oauth_parameters = twitter_sign.get_oauth_parameters(self.consumerKey, self.accessToken)
+        oauth_parameters['oauth_signature'] = twitter_sign.generate_signature(method, url, url_parameters, oauth_parameters, self.consumerKey, self.consumerSecret, self.tokenSecret)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': twitter_sign.create_auth_header(oauth_parameters)}
+        response = requests.get(url, headers=headers, params=url_parameters)
+        print response.status_code
+        print response.json()['response']['timeline_id']
+
+    def getHomeTimeLine(self):
+        method = "get"
+        url = '{}1.1/statuses/home_timeline.json'.format(self.base_url)
+        url_parameters = {
+            'Name': "C",
+            'count': 10,
+            'exclude_replies': True
+        }
+        oauth_parameters = twitter_sign.get_oauth_parameters(self.consumerKey, self.accessToken)
+        oauth_parameters['oauth_signature'] = twitter_sign.generate_signature(method, url, url_parameters, oauth_parameters, self.consumerKey, self.consumerSecret, self.tokenSecret)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': twitter_sign.create_auth_header(oauth_parameters)}
+        response = requests.get(url, headers=headers, params=url_parameters)
+        print response.status_code
+        for i in range(url_parameters['count']):
+            print(i)
+            print response.json()[i]['text'] + '\n'
+
+
+def main():
+    newAuth = TwitterAccess()
+    newAuth.getHomeTimeLine()
+
+if __name__ == '__main__':
+    main()
