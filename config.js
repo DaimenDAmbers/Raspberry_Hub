707f04e00cb6a6ab374c9eb68e8132b75d1943ac
DaimenAmbers <daimenambers@yahoo.com>
2020-08-30 12:56:45 -0400

    Cleaned up files

--- config.js
+++
@@ -1,8 +0,0 @@
-//config.js
-/** TWITTER APP CONFIGURATION*/
-module.exports = {
-  consumer_key: "Zi2YZlt5GrkFdnPDAQM840sYi",
-  consumer_secret: "WbeHhAF5utlGnJ4lY1ELIf4lnFKbDJmut8OPUsxiJNWi5G3mHS",
-  access_token: "2200974972-xyb9647TzntMqtGseJPGJXs2cT5Ahv0YWwtRqkL",
-  access_token_secret: "tYHwDwLnl1AV56ENC13YkW7NDjllM5NcVmsCX6L4sy9E9",
-};
